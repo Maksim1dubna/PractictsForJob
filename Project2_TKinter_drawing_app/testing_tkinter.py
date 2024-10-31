a864fa70a072b8d64ba622a3dab281fcc8aefb5e
Maksim1dubna <68699608+Maksim1dubna@users.noreply.github.com>
2024-10-31 10:16:02 +0300

    Задача №1. Реализовать функционал: Выбор размера кисти из списка

---
+++ Project2_TKinter_drawing_app/testing_tkinter.py
@@ -0,0 +1,12 @@
+# Внимательно пересмотреть https://www.youtube.com/watch?v=6tNS--WetLI и составить тесты.
+import unittest
+class TestTkinetr(unittest.TestCase):
+    def setUp(self):
+        return 0
+
+    def test_task_avg_price_1(self, data):
+        return 0
+
+
+if __name__ == '__main__':
+    unittest.main()
