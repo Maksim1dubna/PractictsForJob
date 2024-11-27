6ddcc4f6e0fab1272709eeda5b80e9d0695454ed
Maksim1dubna <68699608+Maksim1dubna@users.noreply.github.com>
2024-11-28 01:48:24 +0300

    Задача №1. Реализовать функционал: Правка объявлени

---
+++ Project5_Djangoboard/urban_project/board/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'board'
+
+urlpatterns = [
+    path('', views.advertisement_list, name='advertisement_list'),
+    path('edit/', views.advertisement_list_to_edit, name='advertisement_list_to_edit'),
+    path('edit/advertisement/<int:pk>/', views.advertisement_detail_edit, name='advertisement_detail_edit'),
+    path('advertisement/<int:pk>/', views.advertisement_detail, name='advertisement_detail'),
+    path('add/', views.add_advertisement, name='add_advertisement'),
+]
---
+++ Project5_Djangoboard/urban_project/board/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, redirect
+from board.models import Advertisement
+from board.forms import AdvertisementForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login, authenticate
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/board')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def advertisement_list(request):
+    advertisements = Advertisement.objects.all()
+    return render(request, 'board/advertisement_list.html', {'advertisements': advertisements})
+
+def advertisement_detail(request, pk):
+    advertisement = Advertisement.objects.get(pk=pk)
+    return render(request, 'board/advertisement_detail.html', {'advertisement': advertisement})
+
+def advertisement_list_to_edit(request):
+    advertisements = Advertisement.objects.all()
+    return render(request, 'board/advertisement_list_to_edit.html', {'advertisements': advertisements})
+def advertisement_detail_edit(request, pk):
+    advertisement = Advertisement.objects.get(pk=pk)
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST)
+        if form.is_valid():
+            advertisement.title = form.cleaned_data['title']
+            advertisement.content = form.cleaned_data['content']
+            advertisement.author = form.cleaned_data['author']
+            advertisement.save()
+            return redirect('board:advertisement_list_to_edit')
+    else:
+        form = AdvertisementForm()
+        form.fields['title'].initial = advertisement.title
+        form.fields['content'].initial = advertisement.content
+        form.fields['author'].initial = advertisement.author
+    return render(request, 'board/advertisement_detail_edit.html', {'form': form})
+
+
+@login_required
+def add_advertisement(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST)
+        if form.is_valid():
+            advertisement = form.save(commit=False)
+            advertisement.author = request.user
+            advertisement.save()
+            return redirect('board:advertisement_list')
+    else:
+        form = AdvertisementForm()
+    return render(request, 'board/add_advertisement.html', {'form': form})
